d1a8e497394a290a6831f6ac1c8ae4a0b1f41e8a
Grzegorz Krajewski <krajekg@gmail.com>
2025-05-08 18:29:39 +0200

    Add base class

---
+++ scraper_base.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+class RealEstateScraper(ABC):
+    @abstractmethod
+    def scrape(self) -> list[dict]:
+        """Zwraca listę ofert jako słowniki."""
+        pass
+
+    def date_now():
+        # Pobranie bieżącej daty i godziny
+        now = datetime.now()
+        return now.strftime("%H-%d-%m-%Y")  # Format: godzina-dzień-miesiąc-rok
